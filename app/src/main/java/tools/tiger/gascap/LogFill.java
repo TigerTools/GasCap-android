f19bc5c4c227dcf0d16534b69101388abdec32a6
Jesse Greathouse <jesse.greathouse@gmail.com>
2014-12-12 09:05:00 -0600

    changes to play services and location activity

--- app/src/main/java/tools/tiger/gascap/LogFill.java
+++ app/src/main/java/tools/tiger/gascap/LogFill.java
@@ -1,15 +1,30 @@
 package tools.tiger.gascap;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.android.volley.Response;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -20,12 +35,12 @@ import java.util.ArrayList;
 import tools.tiger.gascap.app.GasApi;
 
 
-public class LogFill extends Activity {
+public class LogFill extends FragmentActivity {
 
     public final static String API_TOKEN = "tools.tiger.gascap.API_TOKEN";
+    private GoogleMap mMap;
     public String apiToken;
     public JSONArray vehicles;
-    public JSONArray locations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +48,15 @@ public class LogFill extends Activity {
         Intent intent = getIntent();
         apiToken = intent.getStringExtra(Home.API_TOKEN);
         setContentView(R.layout.activity_log_fill);
+        setUpMapIfNeeded();
         Response.Listener vehiclesListener = getVehiclesListener();
         GasApi.getVehiclesRequest(apiToken, vehiclesListener);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
     }
 
     private Response.Listener<JSONArray> getVehiclesListener() {
@@ -64,13 +86,7 @@ public class LogFill extends Activity {
         vehicleSpinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, vehicleNames));
     }
 
-    public void addLocation() {
-        Intent locationIntent = new Intent(this, Home.class);
-        locationIntent.putExtra(API_TOKEN, apiToken);
-        startActivity(locationIntent);
-    }
-
-    public void addVehicle() {
+    public void addVehicle(View view) {
         Intent vehicleIntent = new Intent(this, Vehicle.class);
         vehicleIntent.putExtra(API_TOKEN, apiToken);
         startActivity(vehicleIntent);
@@ -93,5 +109,43 @@ public class LogFill extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p>
+     * If it isn't installed {@link com.google.android.gms.maps.SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.location_map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
 }
